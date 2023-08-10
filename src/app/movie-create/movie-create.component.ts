bfeceb22aae1de998afa743912cbb2016e8768db
Emirhan <emirhannkilicaslann@gmail.com>
2023-08-10 13:26:49 +0300

    movie-create component updated.

--- src/app/movie-create/movie-create.component.ts
+++ src/app/movie-create/movie-create.component.ts
@@ -4,7 +4,7 @@ import { Category } from '../models/category';
 import { MovieService } from '../services/movie.service';
 import { Router } from '@angular/router';
 import { AlertifyService } from '../services/alertify.service';
-import { NgForm } from '@angular/forms';
+import { FormControl, FormGroup, NgForm, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-movie-create',
@@ -30,19 +30,39 @@ export class MovieCreateComponent implements OnInit{
     })
   }
 
+  movieForm = new FormGroup({
+    title: new FormControl("", [Validators.required, Validators.minLength(5)]),
+    description: new FormControl("", [Validators.required]),
+    imageURL: new FormControl("", [Validators.required]),
+    categoryId: new FormControl("", [Validators.required])
+  })
+
+  get title(){
+    return this.movieForm.get("title");
+  }
+
+  clearForm(){
+    this.movieForm.patchValue({
+      title: "",
+      description: "",
+      imageURL: "",
+      categoryId: "" 
+    })
+  }
+
   createMovie(){
 
-    const movie = {
-      id: 0,
-      title: this.model.title,
-      description: this.model.description,
-      imageURL: this.model.imageURL,
-      isPopular: false,
-      datePublished: new Date().getTime(),
-      categoryId: this.model.categoryId
-    };
+    // const movie = {
+    //   id: 0,
+    //   title: this.model.title,
+    //   description: this.model.description,
+    //   imageURL: this.model.imageURL,
+    //   isPopular: false,
+    //   datePublished: new Date().getTime(),
+    //   categoryId: this.model.categoryId
+    // };
 
-    this.movieService.createMovie(movie).subscribe(data=> this.router.navigate(['/movies', data.id]));
+    // this.movieService.createMovie(movie).subscribe(data=> this.router.navigate(['/movies', data.id]));
   }
 
   log(value: any){
